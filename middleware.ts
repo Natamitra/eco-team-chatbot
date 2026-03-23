33061c1d4eb46e506dc8aea11338cc017b911f54
Natallia Mitrafanava <n.mitrafanava@eco-team.net>
2026-03-23 16:06:53 +0100

    Fix: middleware sesja

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,24 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const session = request.cookies.get("better-auth.session_token");
   const isLoginPage = request.nextUrl.pathname === "/login";
   const isAuthApi = request.nextUrl.pathname.startsWith("/api/auth");
 
   if (isAuthApi) return NextResponse.next();
+
+  const session =
+    request.cookies.get("better-auth.session_token") ||
+    request.cookies.get("session_token") ||
+    request.cookies.get("__Secure-better-auth.session_token");
+
   if (!session && !isLoginPage) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
   if (session && isLoginPage) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
   return NextResponse.next();
 }
 
